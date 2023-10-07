e145c8cf8675907387fe8044f1fa6960676e3992
7emadaz88 <eng.ahmed.abdelfattah@hotmail.com>
2023-10-07 08:36:17 -0700

    +

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: 'prints specific text'
+ *
+ * Return: Always 0 (success)
+ */
+
+int main(void)
+{
+	char T;
+
+	for (T = 'a'; T <= 'z'; T++)
+	{
+		putchar(T);
+		putchar('\n');
+	}
+	return (0);
+}
+
