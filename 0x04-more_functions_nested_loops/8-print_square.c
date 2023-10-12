1e0b2b2c1fb32720b7919e27920625950f734485
7emadaz88 <eng.ahmed.abdelfattah@hotmail.com>
2023-10-12 04:01:14 -0700

    +

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * print_square - entry point
+ * Description: prints square using #
+ * Return: void
+*/
+
+void print_Square(int size)
+{
+	int i, j;
+
+	if (size > 0)
+	{
+		for (i = 0; i < size; i++)
+		{
+			for (j = 0; j <= i; j++)
+			{
+				_putchar('#');
+			}
+			_putchar('\n');
+		}
+	}
+	else
+	{
+		_putchar('\n');
+	}
+}
