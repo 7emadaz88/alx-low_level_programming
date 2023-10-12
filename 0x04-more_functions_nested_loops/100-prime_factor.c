ced9ea9ded0337f6320a00b859652e9e215ab9f5
7emadaz88 <eng.ahmed.abdelfattah@hotmail.com>
2023-10-12 10:24:08 -0700

    +

---
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ * Description: finds biggest prime factor
+ * Return: 0
+*/
+
+int main(void)
+{
+	long n = 612852475143;
+	int i, c;
+
+	for (i = 2; i <= n; i++)
+	{
+		if(n % i == 0)
+		{
+			n = n / i;
+			c = i;
+			i = 1;
+			if(n / c == 1)
+			{
+				i = c;
+			}
+		}
+	}
+	printf("%d\n", c);
+	return (0);
+}
