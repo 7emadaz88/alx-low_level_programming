6cb67c81e49b04cd6cdc51c40f583c03b18323c4
7emadaz88 <eng.ahmed.abdelfattah@hotmail.com>
2023-10-18 10:22:18 -0700

    +

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,42 @@
+#include "main.h"
+#include "xstrlen.c"
+
+/**
+ * _strncpy - entry point
+ * Description: appends src to dest
+ * @src: user input
+ * @dest: user destination
+ * @n: number of characters
+ * Return: string
+*/
+
+char *_strncpy(char *dest, char *src, int n)
+{
+	int i = 0;
+	int j = 0;
+
+	while (i >= 0)
+	{
+		if (dest[i] != '\0')
+		{
+			j++;
+			i++;
+		}
+		else
+		{
+			break;
+		}
+	}
+	for (i = 0; i < n; i++)
+	{
+		dest[i] = src[i];
+	}
+	if (n > _strlen(src))
+	{
+		for (i = _strlen(src) + 1; i < n; i++)
+		{
+			dest[i] = 0x00;
+		}
+	}
+	return (dest);
+}
--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -1,3 +1,4 @@
 int _strlen(char *);
 char *_strcat(char *, char *);
 char *_strncat(char *, char *, int);
+char *_strncpy(char *, char *, int);
