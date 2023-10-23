dbd33773ef09e3e01101d4703f76e0a152bdf7f9
7emadaz88 <eng.ahmed.abdelfattah@hotmail.com>
2023-10-23 17:05:30 +0300

    +

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,25 @@
+#include "main.h"
+#include "xstrlen.c"
+
+/**
+ * _strchr - entry point
+ * Description: fills the first n bytes of the memory area pointed to by s
+ * @s: user input
+ * @c: user input
+ * Return: string
+*/
+
+char *_strchr(char *s, char c)
+{
+    while (*s)
+    {
+        if(*s != c)
+            s++;
+        else
+            break;
+    }
+    if (c == '\0')
+        return (s);
+
+    return (s);
+}
