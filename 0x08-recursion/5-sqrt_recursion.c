ac82b06d5532d9a867f859f41ccee52f2e736c6d
7emadaz88 <eng.ahmed.abdelfattah@hotmail.com>
2023-10-26 01:37:20 +0300

    +

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -3,9 +3,9 @@
 int finder_sqrt_recursion(int n, int i);
 
 /**
- * _sqrt_recursion - returns the natural square root of a number
+ * _sqrt_recursion - entry point
+ * Description: square root finder
  * @n: number to calculate the square root of
- *
  * Return: the resulting square root
  */
 int _sqrt_recursion(int n)
@@ -15,6 +15,13 @@ int _sqrt_recursion(int n)
 	return (finder_sqrt_recursion(n, 0));
 }
 
+/**
+ * finder_sqrt_recursion - entry point
+ * Description: square root of a number
+ * @n: number to calculate the sqaure root of
+ * @i: testing integer
+ * Return: the resulting square root
+ */
 int finder_sqrt_recursion(int n, int i)
 {
 	if (i * i > n)
