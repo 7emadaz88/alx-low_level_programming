8bee84b8bfcfaf20b46cf305e1977d4520a856ff
7emadaz88 <eng.ahmed.abdelfattah@hotmail.com>
2023-10-26 01:40:06 +0300

    +

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,32 +1,32 @@
 #include "main.h"
 
-int finder_sqrt_recursion(int n, int i);
+int finder_sqrt(int n, int i);
 
 /**
  * _sqrt_recursion - entry point
  * Description: square root finder
  * @n: number to calculate the square root of
  * Return: the resulting square root
- */
-int _sqrt_recursion(int n)
-{
-	if (n < 0)
-		return (-1);
-	return (finder_sqrt_recursion(n, 0));
-}
-
-/**
- * finder_sqrt_recursion - entry point
+ *
+ * finder_sqrt - entry point
  * Description: square root of a number
  * @n: number to calculate the sqaure root of
  * @i: testing integer
  * Return: the resulting square root
- */
-int finder_sqrt_recursion(int n, int i)
+*/
+
+int _sqrt_recursion(int n)
+{
+	if (n < 0)
+		return (-1);
+	return (finder_sqrt(n, 0));
+}
+
+int finder_sqrt(int n, int i)
 {
 	if (i * i > n)
 		return (-1);
 	if (i * i == n)
 		return (i);
-	return (finder_sqrt_recursion(n, i + 1));
+	return (finder_sqrt(n, i + 1));
 }
