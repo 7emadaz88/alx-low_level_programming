67b69094cdce857f55c6de59a52383b937848ebd
7emadaz88 <eng.ahmed.abdelfattah@hotmail.com>
2023-10-27 19:47:05 +0200

    +

--- 0x09-static_libraries/main.h
+++ 0x09-static_libraries/main.h
@@ -1,3 +1,6 @@
+#ifndef _MAIN_H_
+#define _MAIN_H_
+
 int _putchar(char);
 int _islower(int);
 int _isalpha(int);
@@ -18,3 +21,5 @@ char *_strchr(char *, char);
 unsigned int _strspn(char *, char *);
 char *_strpbrk(char *, char *);
 char *_strstr(char *, char *);
+
+#endif
