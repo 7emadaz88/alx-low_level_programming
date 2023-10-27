598c5ecd0dd137897dc7e9ada98e3efbbf90d6bf
7emadaz88 <eng.ahmed.abdelfattah@hotmail.com>
2023-10-27 12:51:28 -0700

    +

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,5 +1,4 @@
 #include <stdio.h>
-
 /**
  * main - entry point
  * prints program name
@@ -7,9 +6,8 @@
  * @argv: argument array
  * Return: zero
 */
-
 int main(int argc, char *argv[])
 {
-    printf("%s\n", argv[0]);
-    return (0);
+	printf("%s\n", argv[0]);
+	return (0);
 }
