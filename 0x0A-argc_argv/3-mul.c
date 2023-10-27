65883a1aa8abd831177c5ff49a8fc1d46acfe841
7emadaz88 <eng.ahmed.abdelfattah@hotmail.com>
2023-10-27 14:28:56 -0700

    +

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -9,17 +9,17 @@
 */
 int main(int argc, char *argv[])
 {
-    int i;
+   	int i;
 
-    if (argc != 3)
-    {
-        printf("Error\n");
-        return (1);
-    }
-    else
-    {
-        i = (atoi(argv[1])) * (atoi(argv[2]));
-        printf("%i\n", i);
-        return (0);
-    }
+	if (argc != 3)
+	{
+		printf("Error\n");
+		return (1);
+	}
+	else
+	{
+		i = (atoi(argv[1])) * (atoi(argv[2]));
+		printf("%i\n", i);
+		return (0);
+	}
 }
