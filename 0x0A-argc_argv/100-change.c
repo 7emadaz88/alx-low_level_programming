ab3135ed8852c6aa0d390830a745fc4d04866f0c
7emadaz88 <eng.ahmed.abdelfattah@hotmail.com>
2023-10-27 15:29:24 -0700

    +

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -9,50 +9,50 @@
 */
 int main(int argc, char *argv[])
 {
-    int x, c = 0;
+	int x, c = 0;
 
-    if (argc != 2)
-    {
-        printf("Error\n");
-        return (1);
-    }
-    x = atoi(argv[2]);
-    if (x < 1)
-    {
-        printf("0\n");
-        return (0);
-    }
-    else
-    {
-        while (x > 0)
-        {
-            if (x % 25 == 0)
-            {
-                c++;
-                x = x - 25;
-            }
-            else if (x % 10 == 0)
-            {
-                c++;
-                x = x - 10;
-            }
-            else if (x % 5 == 0)
-            {
-                c++;
-                x = x - 5;
-            }
-            else if (x % 2 == 0)
-            {
-                c++;
-                x = x - 2;
-            }
-            else
-            {
-                c++;
-                x = x - 1;
-            }
-        }
-    }
-    printf("%i", c);
-    return (0);
+	if (argc != 2)
+	{
+		printf("Error\n");
+		return (1);
+	}
+	x = atoi(argv[2]);
+	if (x < 1)
+	{
+		printf("0\n");
+		return (0);
+	}
+	else
+	{
+		while (x > 0)
+		{
+			if (x % 25 == 0)
+			{
+				c++;
+				x = x - 25;
+			}
+			else if (x % 10 == 0)
+			{
+				c++;
+				x = x - 10;
+			}
+			else if (x % 5 == 0)
+			{
+				c++;
+				x = x - 5;
+			}
+			else if (x % 2 == 0)
+			{
+				c++;
+				x = x - 2;
+			}
+			else
+			{
+				c++;
+				x = x - 1;
+			}
+		}
+	}
+	printf("%i", c);
+	return (0);
 }
