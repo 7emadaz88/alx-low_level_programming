8e3620d6d7ad1918ccd32a136c05d1ff718c83a7
7emadaz88 <eng.ahmed.abdelfattah@hotmail.com>
2023-10-27 19:06:29 +0200

    +

---
+++ 0x09-static_libraries/1-strncat.c
@@ -0,0 +1,39 @@
+#include "main.h"
+#include "2-strlen.c"
+
+/**
+ * _strncat - entry point
+ * Description: appends src to dest
+ * @src: user input
+ * @dest: user destination
+ * @n: number of characters
+ * Return: string
+*/
+
+char *_strncat(char *dest, char *src, int n)
+{
+	int i = 0;
+	int j = 0;
+
+	while (i >= 0)
+	{
+		if (dest[i] != '\0')
+		{
+			j++;
+			i++;
+		}
+		else
+		{
+			break;
+		}
+	}
+	if (n > _strlen(src))
+	{
+		n = _strlen(src);
+	}
+	for (i = j; i < n + j; i++)
+	{
+		dest[i] = src[i - j];
+	}
+	return (dest);
+}
---
+++ 0x09-static_libraries/100-atoi.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * _atoi - entry point
+ * Description: atoi
+ * @s: user input
+ * Return: int
+*/
+
+int _atoi(char *s)
+{
+    return (0);
+}
