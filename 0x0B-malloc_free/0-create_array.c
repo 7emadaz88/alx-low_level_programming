541800c01c0feb9da8222837cb2ea60acdc6651e
7emadaz88 <eng.ahmed.abdelfattah@hotmail.com>
2023-10-30 18:21:31 +0200

    +

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,22 @@
+#include <stdlib.h>
+#include <main.h>
+/**
+ * create_array - entry point
+ * creates array of chars
+ * return: char
+*/
+char *create_array(unsigned int size, char c)
+{
+    char *x = malloc(size * sizeof(char));
+    int i;
+
+    if (size == 0)
+    {
+        return ('\0');
+    }
+    for (i = 0; i < size; i++)
+    {
+        x[i] = c;
+    }
+    return (x);
+}
