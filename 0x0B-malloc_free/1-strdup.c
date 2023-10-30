4b825fa8c6bb8e86f4122f1a95a94a4a706353a6
7emadaz88 <eng.ahmed.abdelfattah@hotmail.com>
2023-10-30 09:52:36 -0700

    +

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -1,27 +1,26 @@
 #include <stdlib.h>
 #include "main.h"
 /**
- * strdup - entry point
- * creates a copy of the string given as a parameter
- * @size: user input
- * @c: user input
+ * _strdup - entry point
+ * Description: creates a copy of the string given as a parameter
+ * @str: user input
  * Return: pointer
 */
 char *_strdup(char *str)
 {
-    char *x;
-    int i, l = 0;
+	char *x;
+	int i, l = 0;
 
-    while (str[l] != '\0')
-        l++;
+	while (str[l] != '\0')
+		l++;
 
-    x = malloc((l * sizeof(char)) + 1);
-    if (x == NULL)
-        return (NULL);
+	x = malloc((l * sizeof(char)) + 1);
+	if (x == NULL)
+		return (NULL);
 
-    for (i = 0; i < l; i++)
-    {
-        x[i] = str[i];
-    }
-    return (x);
+	for (i = 0; i < l; i++)
+	{
+		x[i] = str[i];
+	}
+	return (x);
 }
