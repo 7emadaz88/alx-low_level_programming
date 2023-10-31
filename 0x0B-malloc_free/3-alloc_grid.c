53b75d27e8e706b94b1353c8a9e8f4550950601e
7emadaz88 <eng.ahmed.abdelfattah@hotmail.com>
2023-10-31 13:50:17 +0200

    +

---
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -0,0 +1,32 @@
+#include <stdlib.h>
+#include "main.h"
+/**
+* alloc_grid - entry point
+* makes a 2 dimentional array
+* @width: user input
+* @height: user input
+* Return: int pointer to 2 dimentional array
+*/
+int **alloc_grid(int width, int height)
+{
+	int **x;
+	int *w, *h;
+	int i, j;
+
+	if (width < 1 || height < 1)
+		return (NULL);
+
+	x = (int **) malloc(height * sizeof(int));
+	x[*h] = (int *) malloc(width * sizeof(int));
+
+	if (x == NULL)
+		return (NULL);
+
+	for (i = 0; i < height; i++)
+	{
+		for (j = 0; j < width; j++)
+			x[i][j] = 0;
+	}
+
+	return (x);
+}
--- 0x0B-malloc_free/main.h
+++ 0x0B-malloc_free/main.h
@@ -1,3 +1,9 @@
+#ifndef _MAIN_H_
+#define _MAIN_H_
+
 char *create_array(unsigned int, char);
 char *_strdup(char *);
 char *str_concat(char *, char *);
+int **alloc_grid(int, int);
+
+#endif
