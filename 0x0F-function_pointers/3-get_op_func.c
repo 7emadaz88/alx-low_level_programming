80f295523b63511deb776e8446449b2dc3883373
7emadaz88 <eng.ahmed.abdelfattah@hotmail.com>
2023-11-08 22:43:44 +0200

    +

--- 0x0F-function_pointers/3-get_op_func.c
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -19,12 +19,12 @@ int (*get_op_func(char *s))(int, int)
 	};
 	int i = 0;
 
-	while (i < 5)
+	while (i < 6)
 	{
-		if (s[0] == ops[i]->op)
-			break;
+		if (s[0] == ops[i].op)
+			return (ops[i].f);
 		i++;
 	}
 
-	return (ops[i].f);
+	return (NULL);
 }
